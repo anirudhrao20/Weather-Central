a9ad82c788941f126c051768498a0d98e58690db
Anirudh Rao <anirudhrao20@gmail.com>
2023-02-15 09:03:59 -0800

    Initial Commit

---
+++ Weather Central/Weather_CentralApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_CentralApp.swift
+//  Weather Central
+//
+//  Created by Anirudh Rao on 2/15/23.
+//
+
+import SwiftUI
+
+@main
+struct Weather_CentralApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
